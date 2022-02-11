89c16a48bec8842635a0315623327be00597f27f
Itrio <alfredo_lg17@hotmail.com>
2022-02-11 12:04:24 -0700

    Agregando nuevos modelos

--- portafolio/portfolio/admin.py
+++ portafolio/portfolio/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Technologies, Contacto, Experience
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(Technologies)
+admin.site.register(Contacto)
+admin.site.register(Experience)
--- portafolio/portfolio/models.py
+++ portafolio/portfolio/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.fields import CharField, URLField
 from django.db.models.fields.files import ImageField
+import datetime
 
 # Create your models here.
 class Project(models.Model):
@@ -8,3 +9,23 @@ class Project(models.Model):
     description = CharField(max_length=250)
     image = ImageField(upload_to="portfolio/images/")
     url = URLField(blank=True)
+
+
+class Technologies(models.Model):
+    title = CharField(max_length=100)
+    image = ImageField(upload_to="tech/images")
+
+
+class Contacto(models.Model):
+    email = CharField(max_length=100)
+    number = CharField(max_length=100)
+    linkedin = URLField(blank=True)
+    github = URLField(blank=True)
+
+
+class Experience(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length=1000)
+    date_init = models.DateField(datetime.date)
+    date_end = models.DateField(datetime.date)
+    image = ImageField(upload_to="Experience/images")
